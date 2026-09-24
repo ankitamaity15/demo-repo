f6b3fc024712e3e3a0a32621d30201d77fd2fd9f
Ankita Maity <104030259+ankitamaity15@users.noreply.github.com>
2026-09-24 15:02:46 +0530

    Add new variables for deployment and GitHub integration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,32 @@ variable "snowflake_user" {
 variable "snowflake_private_key" {
   type      = string
   sensitive = true
+}
+ 
+variable "deployment_role" {
+  type        = string
+  description = "Role used by Terraform"
+}
+ 
+variable "environment" {
+  type = string
+ 
+  validation {
+    condition     = contains(["DEV", "TEST", "PROD"], var.environment)
+    error_message = "Environment must be DEV, TEST, or PROD."
+  }
+}
+ 
+variable "git_branch" {
+  type = string
+}
+ 
+variable "github_repository_url" {
+  type = string
+}
+ 
+variable "github_token" {
+  type      = string
+  sensitive = true
+}
+
